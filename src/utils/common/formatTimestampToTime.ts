bf7b2f85c22975d67a932933fb50fd7140f42de1
leehyewon0531 <ifpffk@naver.com>
2025-02-10 23:40:04 +0900

    fix: formatTimestampToTime 함수 변경(Timestamp class 객체를 받는 형태)

--- src/utils/common/formatTimestampToTime.ts
+++ src/utils/common/formatTimestampToTime.ts
@@ -1,6 +1,10 @@
-export function formatTimestampToTime(timestamp: string): string {
-  const date = new Date(timestamp);
+export function formatTimestampToTime(timestamp: {
+  seconds: number;
+  nanoseconds: number;
+}): string {
+  const date = new Date(timestamp.seconds * 1000);
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
+
   return `${hours}:${minutes}`;
 }
