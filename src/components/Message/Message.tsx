f792891f598a078cc8f94d16fdce982e11ad72f8
leehyewon0531 <ifpffk@naver.com>
2025-02-11 11:16:41 +0900

    fix: sender 타입 DocumentReference로 변경 + sender.parent.path가 users인지 식별

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,23 +1,21 @@
-import { Timestamp } from "firebase/firestore";
+import { DocumentReference, Timestamp } from "firebase/firestore";
 import { formatTimestampToTime } from "../../utils/common/formatTimestampToTime";
 import styles from "./Message.module.css";
 
 interface MessageProps {
-  sender: string;
+  sender: DocumentReference;
   timestamp: Timestamp;
   content: string;
 }
 
 export default function Message(props: MessageProps) {
   const { sender, timestamp, content } = props;
-  const isUser = sender.includes("user");
+  const isUser = sender.parent.path === "users";
 
   return (
     <div
       className={`${styles["msg-box"]} ${
-        sender.includes("user")
-          ? styles["msg-box-mine"]
-          : styles["msg-box-character"]
+        isUser ? styles["msg-box-mine"] : styles["msg-box-character"]
       }`}
     >
       {isUser && <span>{formatTimestampToTime(timestamp)}</span>}
